b8d4537f455283fc232d2d1682bd403e6810031d
Carter <alt234@gmail.com>
2012-12-07 10:57:31 -0900

    Add asterisks to restart and quit hovers.

--- js/init.js
+++ js/init.js
@@ -12,8 +12,26 @@ $(function () {
 		$("#gameOver").fadeOut(2000);
 	});
 
+    $("#restartText").hover(
+        function () {
+            $(this).html("* Restart *");
+        },
+        function () {
+            $(this).html("Restart");
+        }
+    );
+
     $("#quitText").click(function () {
 	    game.Quit();
         $("#gameOver").fadeOut("slow");
     });
+
+    $("#quitText").hover(
+        function () {
+            $(this).html("* Quit *");
+        },
+        function () {
+            $(this).html("Quit");
+        }
+    );
 });
